bb91ffb553e6105ceab29a9ec100ca74c08c230a
Chris Brackert <cbrackert@gmail.com>
2016-03-15 12:45:27 -0700

    Get Debounce working and passing tests

--- debounce_block.py
+++ debounce_block.py
@@ -1,14 +1,27 @@
+from collections import defaultdict
+from datetime import datetime
 from nio.block.base import Block
+from nio.block.mixins.group_by.group_by import GroupBy
 from nio.util.discovery import discoverable
-from nio.properties import VersionProperty
+from nio.properties import TimeDeltaProperty, VersionProperty
 
 
 @discoverable
-class Debounce(Block):
+class Debounce(GroupBy, Block):
 
+    interval = TimeDeltaProperty(title='Debounce Interval', allow_none=True)
     version = VersionProperty('0.1.0')
 
-    def process_signals(self, signals):
-        for signal in signals:
-            pass
-        self.notify_signals(signals)
+    def __init__(self):
+        super().__init__()
+        self._last_emission = defaultdict(lambda: None)
+
+    def process_group_signals(self, signals, group, input_id):
+        """Check configured interval and return a signal if valid."""
+        now = datetime.utcnow()
+        if self._last_emission[group] is None or \
+                now - self._last_emission[group] > self.interval():
+            self._last_emission[group] = now
+            return signals[:1]
+        else:
+            return []
--- tests/test_debounce_block.py
+++ tests/test_debounce_block.py
@@ -1,3 +1,5 @@
+from time import sleep
+from unittest.mock import MagicMock
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
@@ -6,13 +8,48 @@ from ..debounce_block import Debounce
 
 class TestDebounce(NIOBlockTestCase):
 
-    def test_process_signals(self):
-        blk = Debounce()
-        self.configure_block(blk, {})
-        blk.start()
-        blk.process_signals([Signal()])
-        blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {})
+    def test_debounce(self):
+        """Test that signals are properly debounce for configured interval."""
+        block = Debounce()
+        block._backup = MagicMock()
+        self.configure_block(block, {
+            "interval": {
+                "milliseconds": 200
+            }
+        })
+        block.start()
+        block.process_signals([Signal(), Signal()])
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(1, block)
+        sleep(.3)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(2, block)
+        block.stop()
+
+    def test_debounce_group(self):
+        """Test that the group by mixin is properly used."""
+        block = Debounce()
+        block._backup = MagicMock()
+        self.configure_block(block, {
+            "interval": {
+                "milliseconds": 200
+            },
+            "group_by": "{{ $group }}"
+        })
+        block.start()
+        block.process_signals([
+            Signal({'group': 'bar'}),
+        ])
+        self.assert_num_signals_notified(1, block)
+        block.process_signals([
+            Signal({'group': 'bar'}),
+            Signal({'group': 'foo'})
+        ])
+        self.assert_num_signals_notified(2, block)
+        sleep(.3)
+        block.process_signals([Signal({'group': 'bar'})])
+        self.assert_num_signals_notified(3, block)
+        block.stop()
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][0].group == 'bar')
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][1].group == 'foo')
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][2].group == 'bar')
